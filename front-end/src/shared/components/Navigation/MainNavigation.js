c4b37513c5cd49657b1ee3a1db33ea7da35a27cc
Ranjeet <ranjeetgautam13032@gmail.com>
2021-05-12 19:59:16 +0530

    adding sidedrawer

--- front-end/src/shared/components/Navigation/MainNavigation.js
+++ front-end/src/shared/components/Navigation/MainNavigation.js
@@ -1,25 +1,33 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 import "./MainNavigation.css";
 
 import MainHeader from "./MainHeader";
+import SideDrawer from "./SideDrawer";
 import NavLinks from "./NavLinks";
 
 const MainNavigation = (props) => {
   return (
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to="/">Your Places</Link>
-      </h1>
-      <nav>
-        <NavLinks />
-      </nav>
-    </MainHeader>
+    <Fragment>
+      <SideDrawer>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+      <MainHeader>
+        <button className="main-navigation__menu-btn">
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">Your Places</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </Fragment>
   );
 };
 
