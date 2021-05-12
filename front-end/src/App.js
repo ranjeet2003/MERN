e88af7bf41d3b7c617bfea6216788c93e1ebcdb0
Ranjeet <ranjeetgautam13032@gmail.com>
2021-05-12 18:11:57 +0530

    adding main header

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,18 +8,23 @@ import {
 
 import Users from "./user/pages/Users";
 import NewPlaces from "./places/pages/NewPlaces";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
+
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact>
-          <Users />
-        </Route>
-        <Route path="/places/new">
-          <NewPlaces />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/places/new">
+            <NewPlaces />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
     </Router>
   );
 };
