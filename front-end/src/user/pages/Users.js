2ca015ab6b389c37af3d3a76a8574c9a58da2752
Ranjeet <ranjeetgautam13032@gmail.com>
2021-05-12 11:40:40 +0530

    adding userlist page

--- front-end/src/user/pages/Users.js
+++ front-end/src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return <h2> Users Works! </h2>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Ranjeet",
+      image:
+        "https://png.pngtree.com/png-clipart/20190520/original/pngtree-vector-users-icon-png-image_4144740.jpg",
+      places: 3,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
