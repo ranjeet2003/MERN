9ad6a8f92edc4a18259bfeb87d0caa9c55c2645b
Ranjeet <ranjeetgautam13032@gmail.com>
2021-05-13 00:50:00 +0530

    Animating sidedrawer

--- front-end/src/shared/components/Navigation/MainNavigation.js
+++ front-end/src/shared/components/Navigation/MainNavigation.js
@@ -20,13 +20,13 @@ const MainNavigation = (props) => {
   return (
     <Fragment>
       {drawerIsOpen && <Backdrop onClick={closeDrawer} />}
-      {drawerIsOpen && (
-        <SideDrawer>
-          <nav className="main-navigation__drawer-nav">
-            <NavLinks />
-          </nav>
-        </SideDrawer>
-      )}
+
+      <SideDrawer show={drawerIsOpen}>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+
       <MainHeader>
         <button className="main-navigation__menu-btn" onClick={openDrawer}>
           <span />
