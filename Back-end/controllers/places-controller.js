52a1b45bcd867931fb0ef14512e69b5e1d25a22b
Ranjeet <ranjeetgautam13032@gmail.com>
2021-05-23 22:12:11 +0530

    adding post route

--- Back-end/controllers/places-controller.js
+++ Back-end/controllers/places-controller.js
@@ -1,4 +1,5 @@
 const HttpError = require("../models/http-error");
+const uuid = require("uuid/v4");
 
 const DUMMY_PLACES = [
   {
@@ -44,5 +45,22 @@ const getPlaceByUserId = (req, res, next) => {
   res.json({ place });
 };
 
+const createPlace = (req, res, next) => {
+  const { title, description, coordinates, address, creator } = req.body;
+  const createdPlace = {
+    id: uuid(),
+    title,
+    description,
+    location: coordinates,
+    address,
+    creator,
+  };
+
+  DUMMY_PLACES.push(createdPlace);
+  res.status(201).json({ place: createdPlace });
+  //   console.log(createdPlace);
+};
+
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
+exports.createPlace = createPlace;
--- Back-end/routes/places-routes.js
+++ Back-end/routes/places-routes.js
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get("/:pid", placesController.getPlaceById);
 
 router.get("/user/:uid", placesController.getPlaceByUserId);
+router.post("/", placesController.createPlace);
 
 module.exports = router;
