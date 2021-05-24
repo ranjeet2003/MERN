3be908699b9df67c84a50a9cb68a301077c3b230
Ranjeet <ranjeetgautam13032@gmail.com>
2021-05-24 17:13:25 +0530

    completed rest architecture

--- Back-end/routes/places-routes.js
+++ Back-end/routes/places-routes.js
@@ -1,17 +1,39 @@
-const express = require("express");
+const express = require('express');
+const { check } = require('express-validator');
 
-const placesController = require("../controllers/places-controller");
+const placesControllers = require('../controllers/places-controllers');
 
 const router = express.Router();
 
-router.get("/:pid", placesController.getPlaceById);
+router.get('/:pid', placesControllers.getPlaceById);
 
-router.get("/user/:uid", placesController.getPlacesByUserId);
+router.get('/user/:uid', placesControllers.getPlacesByUserId);
 
-router.post("/", placesController.createPlace);
+router.post(
+  '/',
+  [
+    check('title')
+      .not()
+      .isEmpty(),
+    check('description').isLength({ min: 5 }),
+    check('address')
+      .not()
+      .isEmpty()
+  ],
+  placesControllers.createPlace
+);
 
-router.patch("/:pid", placesController.updatePlace);
+router.patch(
+  '/:pid',
+  [
+    check('title')
+      .not()
+      .isEmpty(),
+    check('description').isLength({ min: 5 })
+  ],
+  placesControllers.updatePlace
+);
 
-router.delete("/:pid", placesController.deletePlace);
+router.delete('/:pid', placesControllers.deletePlace);
 
 module.exports = router;
