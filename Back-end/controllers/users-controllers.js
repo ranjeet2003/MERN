b23426e7f157fea0d756417833a8025ae415425a
Ranjeet <ranjeetgautam13032@gmail.com>
2021-05-30 21:40:59 +0530

    get all users

--- Back-end/controllers/users-controllers.js
+++ Back-end/controllers/users-controllers.js
@@ -14,8 +14,17 @@ const DUMMY_USERS = [
   },
 ];
 
-const getUsers = (req, res, next) => {
-  res.json({ users: DUMMY_USERS });
+const getUsers = async (req, res, next) => {
+  // res.json({ users: DUMMY_USERS });
+
+  let users;
+  try {
+    users = await User.find({}, "-password");
+  } catch (err) {
+    const error = new HttpError("Could not find user !", 500);
+    return next(error);
+  }
+  res.json({ users: users.map((user) => user.toObject({ getters: true })) });
 };
 
 const signup = async (req, res, next) => {
